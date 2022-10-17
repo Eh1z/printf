4d87be5629f8cbe45fa31dc3bcaa744f8081a114
Godsent Ehiz <111048723+Eh1z@users.noreply.github.com>
2022-10-17 20:50:01 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,41 +1,117 @@
-#include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-/**
- * printIdentifiers - prints special characters
- * @next: character after the %
- * @arg: argument for the indentifier
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
+/*
+ * File: _printf.c
+ * Auth: Godsent Oyarekhua
+ *       Emmanuel Ademola
  */
 
-int printIdentifiers(char next, va_list arg)
+#include "main.h"
+
+/**
+ * count_one_bits - Counts the number of bits set
+ *                  to one in a binary number.
+ * @num: The binary number.
+ *
+ * Return: The number of bits set to one.
+ */
+unsigned char count_one_bits(unsigned char num)
 {
-	int functsIndex;
+	unsigned char count = 0;
 
-	identifierStruct functs[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_int},
-		{"i", print_int},
-		{"u", print_unsigned},
-		{"b", print_unsignedToBinary},
-		{"o", print_oct},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"S", print_STR},
-		{NULL, NULL}
-	};
-
-	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
+	while (num != 0)
 	{
-		if (functs[functsIndex].indentifier[0] == next)
-			return (functs[functsIndex].printer(arg));
+		if ((num & 1) == 1)
+			count++;
+		num >>= 1;
 	}
-	return (0);
+
+	return (count);
 }
 
+
+/**
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+
+/**
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, ret = 0;
+	char wid, prec, tmp;
+	unsigned char flag, len;
+	unsigned int (*f)(va_list, buffer_t *,\
+			unsigned char, char, char, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			flag = handle_flags(format + i + 1);
+			tmp = count_one_bits(flag);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1, &tmp);
+			len = handle_length(format + i + tmp + 1);
+			tmp += (len != 0) ? 1 : 0;
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flag, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	buffer_t *output;
+	va_list args;
+	int ret;
+
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
+}
 /**
  * _printf - mimic printf from stdio
  * Description: produces output according to a format
